5d7e1755a312aaba586d8ae8768adef0214e50e8
Hackin7 <zunmun@gmail.com>
2017-06-01 14:47:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from microbit import *
 
+#######################################
 while True:
   presses = button_a.get_presses()
   display.scroll(str(presses))
   sleep(200)
-
+#######################################
 while True: # the microbit is alive
   presses = button_a.get_presses() #
   if presses > 5: 
@@ -13,13 +14,29 @@ while True: # the microbit is alive
       #w we need to know when we've failed
       display.show(Image.ANGRY)
   sleep(2000)
-  
+#######################################  
 while True:
     reading_x = accelerometer.get_x()
     reading_y = accelerometer.get_y()
-    if reading > 20 :
+    if reading_x > 20 :
         display.show("L")
-    elif reading < -20 :
+    elif reading_x < -20 :
         display.show("R")
     else:
         display.show("-")
+#Flappy Bird######################################
+#CONSTANTS - very important
+#these label your values, allowing your code to be more readable and are normally
+#named with 
+DELAY = 20
+FRAMES_PER_WALL+SHIFT = 20
+FRAMES_PER_NEW_WALL = 100
+FRAMES_PER_SCORE = 50
+
+# global variables
+Y = 50     # the bird's (starting) position along the y axis, between 0-99 inclusive (100 numbers)
+speed # this is the speed of your bird ;). There will be a downward acceleration, and downward direction is taken as positive
+score  # the player's score
+frame = 0  # the game's instances
+
+# only the
